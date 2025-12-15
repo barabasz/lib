373bc440522ed3ab3bfbc23fb3031a8ef16ce0f4
Andrzej Barabasz <abarabasz@gmail.com>
2025-12-15 01:37:42 +0100

    update

--- interactive.sh
+++ interactive.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Function to prompt the user for continuation
-# https://raw.githubusercontent.com/barabasz/lib/main/prompt_continue.sh
-
+# Usage: prompt_continue ["Custom question?"]
 prompt_continue() {
-  while true; do
-      if [ -n "$BASH_VERSION" ]; then
-          read -p "Do you want to continue? (Y/N): " yn
-      else
-          read "yn?Do you want to continue? (Y/N): "
-      fi
-      case $yn in
-          # [Yy]* ) echo "You chose to continue."; return 0;;
-          [Yy]* ) return 0;;
-          [Nn]* ) echo "You chose not to continue."; return 1;;
-          * ) echo "Please answer Y/y or N/n.";;
-      esac
-  done
+    local prompt="${1:-Do you want to continue?}"
+    local yn
+    
+    while true; do
+        if [[ -n "$BASH_VERSION" ]]; then
+            read -r -p "$prompt (Y/N): " yn
+        else
+            read -r "yn?$prompt (Y/N): "
+        fi
+        case $yn in
+            [Yy]*) return 0 ;;
+            [Nn]*) echo "Aborted."; return 1 ;;
+            "") ;;
+            *) echo "Please answer Y or N." ;;
+        esac
+    done
 }
